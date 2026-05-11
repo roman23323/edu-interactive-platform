826cc2e9c0a7fcf9aa784306c0c96e0cf7dbcc33
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-11 12:41:49 +0300

    feat: add register and login pages

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import RegisterForm
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('/')
+
+    else:
+        form = RegisterForm()
+
+    return render(request, 'users/register.html', {
+        'form': form
+    })
