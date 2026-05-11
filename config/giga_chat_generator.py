dcd2478673af970d67e96d649899546caffbf8f9
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-11 20:47:14 +0300

    feat: add gigachat quiz generation service

---
+++ config/giga_chat_generator.py
@@ -0,0 +1,46 @@
+import json
+from gigachat import GigaChat
+
+from django.conf import settings
+
+
+
+giga = GigaChat(
+    credentials=settings.GIGACHAT_API_KEY,
+    scope='GIGACHAT_API_PERS',
+    model='GigaChat-2',
+)
+
+
+def generate_quiz_from_gigachat(topic: str) -> dict:
+    
+    prompt = f"""
+    Сгенерируй вопросы для викторины в формате JSON.
+
+    Тема: {topic}
+
+    Требования:
+    - В качестве ответа возвращай ТОЛЬКО валидный JSON (твой ответ будет парситься как JSON)
+    - Никаких дополнительных текстов - только JSON в ответе
+    - Никаких дополнительных знаков, в том числе никакой markdown-разметки
+    - Сгенерировать нужно 5 вопросов
+    - Для каждого вопроса: 4 варината ответа, один из правильных
+
+    Формат выходного JSON:
+    {{
+      "title": "Тема",
+      "questions": [
+        {{
+          "text": "Вопрос",
+          "other_options": ["Вариант1", "Вариант2", "Вариант3", "Вариант4"],
+          "right_answer": "Вариант3"
+        }}
+      ]
+    }}
+    """
+
+    response = giga.chat(prompt)
+
+    content = response.choices[0].message.content[7:-3]
+
+    return json.loads(content)
