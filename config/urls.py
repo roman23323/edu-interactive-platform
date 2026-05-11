2384168c3a7436fbac4eee44da8a480ec4dc0305
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-11 17:42:23 +0300

    feat: include cards urls

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('', include('quizzes.urls'))
+    path('', include('quizzes.urls')),
+    path('cards/', include('cards.urls'))
 ]
